4454456cadbcdecf51f77b39758d683801d25132
ddangme <ssilver121@naver.com>
2026-04-14 16:38:31 +0900

    refactor: 불필요한 import 삭제

--- order/src/main/java/org/order/domain/entity/Order.java
+++ order/src/main/java/org/order/domain/entity/Order.java
@@ -5,7 +5,6 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.order.domain.policy.CustomerOrderStatusPolicy;
 import org.order.domain.policy.OrderStatusPolicy;
 import org.order.domain.vo.Delivery;
 import org.order.domain.vo.Product;
